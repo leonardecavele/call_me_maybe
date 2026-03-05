4b9729ae653ba7123b50fe2a8faa42bf12380f1c
leonardecavele <leonard.decavele@gmail.com>
2026-03-05 01:52:26 +0100

    wip: best result yet (still tweaking constrained decoding)

--- src/decoding.py
+++ src/decoding.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 from enum import IntEnum, auto
 from typing import Any
@@ -17,23 +18,70 @@ class Step(IntEnum):
 
 
 def generate_function_name(
-    model: Small_LLM_Model, prompt_ids: list[int], function_names
+    model: Small_LLM_Model, prompt_ids: list[int]
 ) -> list[int]:
     function_name_ids: list[int] = []
+
+    next_id: int = model.encode("fn_")[0].tolist()[0]
     next_token: str = ""
+
     while "\"" not in next_token:
+        function_name_ids.append(next_id)
+        prompt_ids.append(next_id)
+        logger.debug("next_id=%d piece=%r", next_id, next_token)
+
         logits: list[float] = model.get_logits_from_input_ids(prompt_ids)
-        function_name_ids.append(
-            max(range(len(logits)), key=logits.__getitem__)
-        )
+        next_id = max(range(len(logits)), key=logits.__getitem__)
+        next_token = model.decode([next_id])
+
     return function_name_ids
 
 
 def generate_parameters(
-    model: Small_LLM_Model, prompt_ids: list[int]
+    model: Small_LLM_Model, prompt_ids: list[int],
+    functions: list[dict[str, Any]], TOOL_CALL: int
 ) -> list[int]:
-    logits: list[float] = model.get_logits_from_input_ids(prompt_ids)
-    return [max(range(len(logits)), key=logits.__getitem__)]
+    parameters_ids: list[int] = []
+
+    # TODO PROTECT TYPE HINT ALL
+    function_name: str = re.findall(
+        r'"name":"([^"]+)"', model.decode(prompt_ids)
+    )[-1]
+    fn: dict[str, Any] = next(
+        (f for f in functions if f.get("name") == function_name), {}
+    )
+    parameters: dict[str, dict[str, str]] = fn.get("parameters", {})
+    pattern: list[int] = model.encode(",".join(
+        [f"\"{k}\":\"<tool_call>\"" for k in parameters.keys()]
+    ))[0].tolist()
+
+    for token_id in pattern:
+        if token_id == TOOL_CALL:
+            next_str: str = ""
+            while "\"" not in next_str:
+                logits: list[float] = model.get_logits_from_input_ids(
+                    prompt_ids
+                )
+                next_id: int = max(range(len(logits)), key=logits.__getitem__)
+                next_str: str = model.decode([next_id])
+
+                if "}" in next_str:
+                    break
+
+                logger.debug(
+                    "token_id=%d piece=%r", next_id, next_str
+                )
+                parameters_ids.append(next_id)
+                prompt_ids.append(next_id)
+        else:
+            logger.debug(
+                "token_id=%d piece=%r",
+                token_id, model.decode([token_id])
+            )
+            parameters_ids.append(token_id)
+            prompt_ids.append(token_id)
+
+    return parameters_ids
 
 
 def get_answers(
@@ -49,14 +97,12 @@ def get_answers(
         model.encode(p)[0].tolist() for p in augmented_prompts
     ]
 
-    function_names: list[str] = [f['name'] for f in functions]
-
     for prompt_i, _ in enumerate(prompts_ids):
 
         pattern: list[int] = model.encode(
             f"{{\"prompt\":\"{prompts[prompt_i]}\","
             f"\"name\":\"<tool_call>\","
-            f"\"parameters\":\"{{<tool_call>}}\"}}"
+            f"\"parameters\":{{<tool_call>}}}}"
         )[0].tolist()
 
         step: int = 0
@@ -66,13 +112,15 @@ def get_answers(
                 if step == Step.FUNCTION_NAME:
                     logger.info("generating function name")
                     new_ids += generate_function_name(
-                        model, prompts_ids[prompt_i], function_names
+                        model, prompts_ids[prompt_i]
                     )
+                    step += 1
                 elif step == Step.PARAMETERS:
                     logger.info("generating parameters")
                     new_ids += generate_parameters(
-                        model, prompts_ids[prompt_i]
+                        model, prompts_ids[prompt_i], functions, TOOL_CALL
                     )
+                    step += 1
                 answer_ids += new_ids
             else:
                 logger.debug(
